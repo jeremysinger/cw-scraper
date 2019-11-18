739868abbbbf624b99d7359ad2a36e944ab24933
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2019-11-18 17:52:55 +0000

    refactor scraping tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Test.Tasty
@@ -9,32 +11,22 @@ main :: IO ()
 main = do
   defaultMain (testGroup "scraper library tests"
     [ badUrlScraperTest,
-      randomCountryScraperTest,
-      randomWordScraperTest,
-      getScrapedWordFromWikiPage "USA" "American"
+      getScrapedWordFromWikiPage "Banana" "musa",
+      getScrapedWordFromWikiPage "Ruritania" "zenda",
+      getScrapedWordFromWikiPage "USA" "american",
+      getScrapedWordFromWikiPage "Russia" "soviet"
     ])
 
 getScrapedWordFromWikiPage :: String -> String -> TestTree
 getScrapedWordFromWikiPage article expected =
-  testCase ("testing " ++ article ++ " page")
-    (assertEqual ("most frequent word should be " ++ expected)
+  testCase ("testing " ++ article ++ " page") $ do
+    res <- mostfrequentwordonpage $ "https://en.wikipedia.org/wiki/"++article
+    assertEqual ("most frequent word should be " ++ expected) 
       (Just expected)
-      (mostfrequentwordonpage $ "https://en.wikipedia.org/wiki/"++article))
-
-
+      res
 
 badUrlScraperTest :: TestTree
-badUrlScraperTest = testCase "testing bad url"
-  (assertEqual "should evaluate to Nothing" Nothing
-   (mostfrequentwordonpage "https://nosuchpage.wiki/foo"))
-
-randomCountryScraperTest :: TestTree
-randomCountryScraperTest = testCase "testing Ruritania page"
-  (assertEqual "most frequent word is zenda" (Just "zenda")
-   (mostfrequentwordonpage "https://en.wikipedia.org/wiki/Ruritania"))
-
-randomWordScraperTest :: TestTree
-randomWordScraperTest = testCase "testing Banana page"
-  (assertEqual "most frequent word is musa" (Just "musa")
-   (mostfrequentwordonpage "https://en.wikipedia.org/wiki/Banana"))
+badUrlScraperTest = testCase "testing bad url" $ do
+  res <- mostfrequentwordonpage "https://nosuchpage.wiki/foo"
+  Nothing @=? res
 
