5cecac968d92c5a987af7da42bf8552fbb1219e0
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2019-11-18 16:03:34 +0000

    added some more unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,10 +7,34 @@ import WikiScrapeLib (mostfrequentwordonpage)
 
 main :: IO ()
 main = do
-  defaultMain (testGroup "scraper library tests" [badUrlScraperTest])
+  defaultMain (testGroup "scraper library tests"
+    [ badUrlScraperTest,
+      randomCountryScraperTest,
+      randomWordScraperTest,
+      getScrapedWordFromWikiPage "USA" "American"
+    ])
+
+getScrapedWordFromWikiPage :: String -> String -> TestTree
+getScrapedWordFromWikiPage article expected =
+  testCase ("testing " ++ article ++ " page")
+    (assertEqual ("most frequent word should be " ++ expected)
+      (Just expected)
+      (mostfrequentwordonpage $ "https://en.wikipedia.org/wiki/"++article))
+
+
 
 badUrlScraperTest :: TestTree
 badUrlScraperTest = testCase "testing bad url"
   (assertEqual "should evaluate to Nothing" Nothing
    (mostfrequentwordonpage "https://nosuchpage.wiki/foo"))
+
+randomCountryScraperTest :: TestTree
+randomCountryScraperTest = testCase "testing Ruritania page"
+  (assertEqual "most frequent word is zenda" (Just "zenda")
+   (mostfrequentwordonpage "https://en.wikipedia.org/wiki/Ruritania"))
+
+randomWordScraperTest :: TestTree
+randomWordScraperTest = testCase "testing Banana page"
+  (assertEqual "most frequent word is musa" (Just "musa")
+   (mostfrequentwordonpage "https://en.wikipedia.org/wiki/Banana"))
 
